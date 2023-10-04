29054f89c1ea4be13cf5587e3e494921b41b04aa
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-10-04 08:05:29 +0300

    Add model field Flat.new_building.

--- property/models.py
+++ property/models.py
@@ -46,6 +46,8 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+    
+    new_building = models.BooleanField(verbose_name="Новостройка", null=True, )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
